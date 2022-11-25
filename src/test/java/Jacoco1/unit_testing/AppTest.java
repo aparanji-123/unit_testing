c7d181b6dcf4741a8602dd9031983b10d6005619
APARANJI <APARANJI@LAPTOP-NVIS5OT0>
2022-11-25 21:55:37 +0530

    This is Unit testing

---
+++ src/test/java/Jacoco1/unit_testing/AppTest.java
@@ -0,0 +1,21 @@
+package Jacoco1.unit_testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test()
+    {
+    	App p=new App();
+        assertEquals(20,p.add(10,10));
+    }
+}
